cfba0325153aa87db2853fab08007407af14838c
delmaSong <sdy2856@gmail.com>
2023-06-10 16:25:50 +0900

    chore: 계층구조 수정

---
+++ iOSAppDevTutorials/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Delma Song on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
